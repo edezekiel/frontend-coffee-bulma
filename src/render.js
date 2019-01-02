ac4f8ef1bb1728a599f2f035a42d283d808a0e92
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-01-02 11:08:23 -0500

    draft renderRefresh function

--- src/render.js
+++ src/render.js
@@ -2,11 +2,17 @@ function renderTitles(){
   const panel = document.querySelector(".list-panel")
   const ul = document.createElement('ul')
   panel.appendChild(ul)
-  // renderRefresh(ul)
+  renderRefresh(ul)
   getBlends()
   .then( blends => blends.forEach(
     blend => renderBlendTitle(blend)
   ))
+}
+
+function renderRefresh(ul) {
+  ul.innerHTML = ""
+  const show = document.querySelector(".show-panel")
+  show.innerHTML = ""
 }
 
 function renderBlendTitle(blend) {
